eca3756f3e8ede53d6cae0551a6e46e999f17c02
sean-tan-columbia <dsp.columbus.2018@gmail.com>
2025-08-18 14:01:27 -0400

    Use local variables

--- environments/essay-sensei-ai/dev/main.tf
+++ environments/essay-sensei-ai/dev/main.tf
@@ -1,15 +1,20 @@
+locals {
+    namespace   = "essay_sensei_ai"
+    environment = "dev"
+}
+
 module auth_gateway {
     source      = "../../../modules/auth-gateway"
 
-    namespace   = "essay_sensei_ai"
-    environment = "dev"
+    namespace   = local.namespace
+    environment = local.environment
 }
 
 module genai_gateway {
     source      = "../../../modules/genai-gateway"
 
-    namespace   = "essay_sensei_ai"
-    environment = "dev"
+    namespace   = local.namespace
+    environment = local.environment
 
     cognito_user_pool_id        = module.auth_gateway.cognito_user_pool_id
     cognito_user_pool_client_id = module.auth_gateway.cognito_user_pool_client_id
