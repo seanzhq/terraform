74054ceb337f625d1808e17c7194bc573fd57d68
sean-tan-columbia <dsp.columbus.2018@gmail.com>
2025-08-18 13:58:49 -0400

    Create stage for auth endpoints

--- modules/auth-gateway/main.tf
+++ modules/auth-gateway/main.tf
@@ -154,3 +154,9 @@ resource "aws_lambda_permission" "allow_api_auth_invoke" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
